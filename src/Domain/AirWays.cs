945bdc21d1e6ac6f94dbdcd1438c98d4c1cf5138
lingme <kid--l@hotmail.com>
2019-08-24 17:49:06 +0800

    add item template

---
+++ src/Domain/AirWays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public enum AirWays
+    {
+        [Description("Flight Way is Arrivals")]
+        A,
+
+        [Description("Flight Way is Deviate")]
+        B
+    }
+}
---
+++ src/Domain/Models/AircraftAR.cs
@@ -0,0 +1,27 @@
+﻿using PropertyChanged;
+using System.Windows;
+using System.Windows.Shapes;
+using Wpf=System.Windows.Media;
+
+namespace Domain.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class AircraftAR
+    {
+        public string FlightNumber { get; set; }
+
+        public AirWays AirWay { get; set; }
+
+        public double FlightX { get; set; }
+
+        public double FlightY { get; set; }
+
+        public double FlightAngle { get; set; }
+
+        public bool IsShowFlightNumber { get; set; }
+
+        public bool IsShowAirWay { get; set; }
+
+        public Wpf::Brush Fill { get; set; }
+    }
+}
